ba467f673f388290aedf28cfa0f80d4b10f8d219
rxb870 <18215428+rxb870@users.noreply.github.com>
2021-02-16 20:09:00 +0000

    DB connected, auth route initialised

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
+
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('API running');
 });
+
+// Define routes
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
